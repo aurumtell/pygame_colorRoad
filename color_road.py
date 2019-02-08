3c177a905a7411ea0cdea0fe981aa13ae59c6f4e
dayana.astana <dayana.astana@gmail.com>
2019-02-08 20:39:27 +0600

    Add moving fenses, rules, collision Dayana and Diana with TeamViewer

--- color_road.py
+++ color_road.py
@@ -6,19 +6,17 @@ import sys
 pygame.init()
 FPS = 50
 fps = 10
-
+fenses = ["black_fense.png", "green_fense.png", "white_fense.png"]
+chickens = ["black_chicken.png", 'green_chicken.png', 'white_chicken.png']
 clock = pygame.time.Clock()
 size = WIDTH, HEIGHT = 300, 500
 screen = pygame.display.set_mode(size)
 running = True
 rule_group = pygame.sprite.Group()
-
 play_group = pygame.sprite.Group()
 start_button = pygame.draw.rect(screen, (0, 0, 240), (25, 130, 245, 50))
 pygame.display.flip()
 all_sprites = pygame.sprite.Group()
-
-
 
 
 def load_image(name, colorkey=None):
@@ -102,6 +100,7 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
         self.rect = self.rect.move(x, y)
+        self.mask = pygame.mask.from_surface(self.image)
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns, sheet.get_height() // rows)
@@ -121,25 +120,37 @@ class Fense(pygame.sprite.Sprite):
         image = pygame.transform.scale(load_image(name), (300, 85))
         super().__init__(all_sprites)
         self.image = image
+        self.name = name
         self.rect = self.image.get_rect()
         # вычисляем маску для эффективного сравнения
         self.rect.x = x
         self.rect.y = y
+        self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
-        self.rect = self.rect.move(0, 3)
+        if pygame.sprite.collide_mask(self, chicken):
+            return True
+        return self.rect.y
+
+
+chicken_color = 0
+chicken = AnimatedSprite(load_image(chickens[chicken_color]), 3, 1, 113, 400)
 
 
 def game_screen():
     screen = pygame.display.set_mode(size)
     running = True
     fenses = ["black_fense.png", "green_fense.png", "white_fense.png"]
+    chickens = ["black_chicken.png", 'green_chicken.png', 'white_chicken.png']
     x = 125
-    count = 0
+    fox = False
+    # count = 0
+    chicken_color = 0
     screen.blit(load_image("game_fon.png"), (0, 0))
-
-    black_chicken = AnimatedSprite(load_image("black_chicken.png"), 3, 1, 113, 400)
-    chicken_group = pygame.sprite.Group(black_chicken)
+    chicken = AnimatedSprite(load_image(chickens[chicken_color]), 3, 1, 113, 400)
+    chicken_group = pygame.sprite.Group(chicken)
+    USEREVENT = 31
+    pygame.time.set_timer(USEREVENT, 12000)
     while running:
         screen.blit(load_image("game_fon.png"), (0, 0))
         for event in pygame.event.get():
@@ -147,21 +158,37 @@ def game_screen():
                 running = False
             keys_pressed = pygame.key.get_pressed()
             if keys_pressed[pygame.K_LEFT]:
-                if WIDTH > (black_chicken.rect[0] - 100) > 0:
-                    black_chicken.rect[0] -= 100
+                if WIDTH > (chicken.rect[0] - 100) > 0:
+                    chicken.rect[0] -= 100
             if keys_pressed[pygame.K_RIGHT]:
-                if (black_chicken.rect[0] + 100) < WIDTH:
-                    black_chicken.rect[0] += 100
-        if count == 20:
-            random_fense = Fense(0, -85, random.choice(fenses))
-            fense_group = pygame.sprite.Group(random_fense)
-            count = 0
-        black_chicken.update()
-        chicken_group.draw(screen)
+                if (chicken.rect[0] + 100) < WIDTH:
+                    chicken.rect[0] += 100
+            if event.type == USEREVENT:
+                fox = True
+                print("fense")
+                chicken_color = random.randint(0, 2)
+                random_fense = Fense(0, -185, fenses[chicken_color])
+                fense_group = pygame.sprite.Group(random_fense)
+                random_fense.update()
+                # if random_fense.update():
+                #     black_chicken = AnimatedSprite(load_image(chickens[chicken_color]), 3, 1, 113, 400)
+                fense_group.draw(screen)
+
+        if fox:
+            if random_fense.update() > 550:
+                fox = False
+                pygame.time.set_timer(USEREVENT, 12000)
+            random_fense.update()
+            fense_group.draw(screen)
+            chicken.update()
+            chicken_group.draw(screen)
+        if fox == False:
+            chicken.update()
+            chicken_group.draw(screen)
         all_sprites.update()
         pygame.display.flip()
         clock.tick(fps)
-        count += 1
+        # count += 1
 
     pygame.quit()
 
@@ -170,10 +197,10 @@ def rule_screen():
     screen = pygame.display.set_mode(size)
     running = True
     intro_text = ["Правила игры", 'Назад']
-    fon = pygame.transform.scale(load_image('rule_fon.jpg'), (WIDTH, HEIGHT))
+    fon = pygame.transform.scale(load_image('rule_fon.png'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     back_button = BackButton(250, 50, intro_text[1], 25, 440)
-    all_sprites.draw(screen)
+    rule_group.draw(screen)
     pygame.display.flip()
     while True:
         for event in pygame.event.get():
@@ -204,7 +231,8 @@ def start_screen():
     screen.blit(text, (text_x, text_y))
     rule_button = Rule_button(250, 50, intro_text[2], 20, 240)
     play_button = Play_button(250, 50, intro_text[3], 20, 330)
-    all_sprites.draw(screen)
+    rule_group.draw(screen)
+    play_group.draw(screen)
     pygame.display.flip()
 
     while True:
