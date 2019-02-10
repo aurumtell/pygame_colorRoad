c98a4468c466619c7dd8a304d9f84ad2515d8b4d
dayana.astana <dayana.astana@gmail.com>
2019-02-10 19:39:29 +0600

    Add collecting eggs Dayana

--- color_road.py
+++ color_road.py
@@ -14,6 +14,7 @@ screen = pygame.display.set_mode(size)
 running = True
 rule_group = pygame.sprite.Group()
 play_group = pygame.sprite.Group()
+exit_group = pygame.sprite.Group()
 start_button = pygame.draw.rect(screen, (0, 0, 240), (25, 130, 245, 50))
 pygame.display.flip()
 all_sprites = pygame.sprite.Group()
@@ -91,6 +92,25 @@ class Play_button(pygame.sprite.Sprite):
             return True
 
 
+class Exit_button(pygame.sprite.Sprite):
+    def __init__(self, width, height, title, x, y):
+        super().__init__(play_group, all_sprites)
+        self.image = pygame.Surface([width, height])
+        self.image.fill(pygame.Color("lightskyblue"))
+        font = pygame.font.Font(None, 50)
+        text = font.render(title, 1, (255, 255, 255))
+        text_x = 65
+        text_y = 7
+        self.image.blit(text, (text_x, text_y))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def check_click(self, mouse):
+        if self.rect.collidepoint(mouse):
+            return True
+
+
 class AnimatedSprite(pygame.sprite.Sprite):
 
     def __init__(self, sheet, columns, rows, x, y):
@@ -130,7 +150,6 @@ class Fense(pygame.sprite.Sprite):
     def update(self):
         self.rect = self.rect.move(0, 3)
         if pygame.sprite.collide_mask(self, chicken):
-            print('True')
             return True
         else:
             return self.rect.y
@@ -141,13 +160,24 @@ class Egg(pygame.sprite.Sprite):
         image = pygame.transform.scale(load_image(name), (100, 85))
         super().__init__(all_sprites)
         self.image = image
+        self.name = name
         self.rect = self.image.get_rect()
         # вычисляем маску для эффективного сравнения
         self.rect.x = x
         self.rect.y = y
+        self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
-        self.rect = self.rect.move(0, 7)
+        global chicken_color
+        global chicken
+        self.rect = self.rect.move(0, 10)
+        if pygame.sprite.collide_mask(self, chicken):
+            if self.name[0] == chickens[chicken_color][0]:
+
+                print('collided')
+                self.kill()
+            # return self.name[0]
+            
         return self.rect.y
 
 
@@ -155,7 +185,19 @@ chicken_color = 0
 chicken = AnimatedSprite(load_image(chickens[chicken_color]), 3, 1, 113, 300)
 
 
+def pause():
+    PAUSED = False
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    PAUSED = not PAUSED
+                    return
+
+
 def game_screen():
+    global chicken
+    global chicken_color
     screen = pygame.display.set_mode(size)
     running = True
     fenses = ["black_fense.png", "green_fense.png", "white_fense.png"]
@@ -164,7 +206,6 @@ def game_screen():
     x = 125
     fense_fox = False
     egg_fox = False
-    # count = 0
     chicken_color = 0
     screen.blit(load_image("game_fon.png"), (0, 0))
     chicken = AnimatedSprite(load_image(chickens[chicken_color]), 3, 1, 113, 300)
@@ -172,12 +213,22 @@ def game_screen():
     USEREVENT = 31
     USEREVENT1 = 23
     pygame.time.set_timer(USEREVENT, 12000)
-    pygame.time.set_timer(USEREVENT1, 5000)
+    pygame.time.set_timer(USEREVENT1, 3000)
+    # pygame.mixer.music.load('odmin/PycharmProjects/yandex/pygame/colorRoad/data/fon.wav')
+    points = 0
+    font = pygame.font.Font(None, 50)
+    text = font.render(str(points), 1, (100, 255, 100))
+    screen.blit(text, (10, 10))
+    pygame.draw.rect(screen, (0, 255, 0), (0, 0, 5, 5))
     while running:
+        # pygame.mixer.music.play()
         screen.blit(load_image("game_fon.png"), (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    pause()
             keys_pressed = pygame.key.get_pressed()
             if keys_pressed[pygame.K_LEFT]:
                 if WIDTH > (chicken.rect[0] - 100) > 0:
@@ -187,9 +238,8 @@ def game_screen():
                     chicken.rect[0] += 100
             if event.type == USEREVENT:
                 fense_fox = True
-                print("fense")
-                chicken_color = random.randint(0, 2)
-                random_fense = Fense(0, -85, fenses[chicken_color])
+                fense_color = random.randint(0, 2)
+                random_fense = Fense(0, -85, fenses[fense_color])
                 fense_group = pygame.sprite.Group(random_fense)
                 random_fense.update()
                 # if random_fense.update():
@@ -215,11 +265,10 @@ def game_screen():
                 egg_group2.draw(screen)
                 egg_group3.draw(screen)
         if egg_fox:
-            if random_egg1.update() > 500:
+            if random_egg2.update() > 500:
                 egg_fox = False
-                pygame.time.set_timer(USEREVENT1, 5000)
-            # random_egg1.update()
-            random_egg2.update()
+                pygame.time.set_timer(USEREVENT1, 2600)
+            random_egg1.update()
             random_egg3.update()
             egg_group1.draw(screen)
             egg_group2.draw(screen)
@@ -230,6 +279,7 @@ def game_screen():
                 fense_fox = False
                 pygame.time.set_timer(USEREVENT, 12000)
             if random_fense.update() == True:
+                chicken_color = fense_color
                 chicken = AnimatedSprite(load_image(chickens[chicken_color]), 3, 1, 113, 300)
                 chicken_group = pygame.sprite.Group(chicken)
                 chicken_group.update()
@@ -254,7 +304,7 @@ def rule_screen():
     intro_text = ["Правила игры", 'Назад']
     fon = pygame.transform.scale(load_image('rule_fon.png'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
-    back_button = BackButton(250, 50, intro_text[1], 25, 440)
+    back_button = BackButton(250, 50, intro_text[1], 25, 340)
     rule_group.draw(screen)
     pygame.display.flip()
     while True:
@@ -275,7 +325,7 @@ def rule_screen():
 def start_screen():
     intro_text = ["COLOR ROAD", "",
                   "Правила игры",
-                  "Играть"]
+                  "Играть", 'Выход']
 
     fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
@@ -284,8 +334,10 @@ def start_screen():
     text_x = 30
     text_y = 30
     screen.blit(text, (text_x, text_y))
-    rule_button = Rule_button(250, 50, intro_text[2], 20, 240)
-    play_button = Play_button(250, 50, intro_text[3], 20, 330)
+    rule_button = Rule_button(250, 50, intro_text[2], 20, 160)
+    play_button = Play_button(250, 50, intro_text[3], 20, 230)
+    exit_button = Exit_button(250, 50, intro_text[4], 20, 300)
+    exit_group.draw(screen)
     rule_group.draw(screen)
     play_group.draw(screen)
     pygame.display.flip()
@@ -308,6 +360,12 @@ def start_screen():
                     rule_button.kill()
                     play_button.kill()
                     game_screen()
+                else:
+                    pass
+
+                if exit_button.check_click(event.pos):
+                    print("clicked")
+                    pygame.quit()
                 else:
                     pass
 
