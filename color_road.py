64c5e220f1a0b861cbc4839be990591a01985ce5
dayana.astana <dayana.astana@gmail.com>
2019-02-13 23:29:16 +0600

    Add points Dayana Add points changing Diana  Add music and fix some bugs Dayana

--- color_road.py
+++ color_road.py
@@ -6,6 +6,9 @@ import sys
 pygame.init()
 FPS = 50
 fps = 10
+points = 0
+count = 0
+levels = {0:[3,10], 1:[5,3]}
 fenses = ["black_fense.png", "green_fense.png", "white_fense.png"]
 chickens = ["black_chicken.png", 'green_chicken.png', 'white_chicken.png']
 clock = pygame.time.Clock()
@@ -122,6 +125,8 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.image = self.frames[self.cur_frame]
         self.rect = self.rect.move(x, y)
         self.mask = pygame.mask.from_surface(self.image)
+        self.x = x
+        self.y = y
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns, sheet.get_height() // rows)
@@ -134,6 +139,10 @@ class AnimatedSprite(pygame.sprite.Sprite):
     def update(self):
         self.cur_frame = (self.cur_frame + 1) % len(self.frames)
         self.image = self.frames[self.cur_frame]
+
+    def getter(self):
+        return self.x, self.y
+
 
 
 class Fence(pygame.sprite.Sprite):
@@ -149,17 +158,19 @@ class Fence(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
-        self.rect = self.rect.move(0, 3)
+        self.rect = self.rect.move(0, 10)
         if pygame.sprite.collide_mask(self, chicken):
+
             return True
         else:
             return self.rect.y
 
 
 class Egg(pygame.sprite.Sprite):
-    def __init__(self, x, y, name):
+    def __init__(self, x, y, name,count):
         image = pygame.transform.scale(load_image(name), (100, 85))
         super().__init__(all_sprites)
+        self.count = count
         self.image = image
         self.name = name
         self.rect = self.image.get_rect()
@@ -169,34 +180,52 @@ class Egg(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
-        global chicken_color
-        global chicken
-        self.rect = self.rect.move(0, 10)
+        global chicken_color, points
+        global chicken, random_fence
+        self.rect = self.rect.move(0, levels[count][1])
         if pygame.sprite.collide_mask(self, chicken):
             if self.name[0] == chickens[chicken_color][0]:
                 self.kill()
+
+                return True
             else:
                 return False
+        if pygame.sprite.collide_mask(self, random_fence):
+            self.rect = self.rect.move(0, 7)
         return self.rect.y
+
 
 
 chicken_color = 0
 chicken = AnimatedSprite(load_image(chickens[chicken_color]), 3, 1, 113, 300)
+fences = ["black_fense.png", "green_fense.png", "white_fense.png"]
+fence_color = random.randint(0, 2)
+random_fence = Fence(0, -85, fences[fence_color])
+egg_sound = pygame.mixer.Sound('data/egg_sound.wav')
 
 
 def pause():
     PAUSED = False
+    pygame.mixer.music.pause()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
+                    pygame.mixer.music.unpause()
                     PAUSED = not PAUSED
                     return
 
 
 def game_screen():
-    global chicken
+    global chicken, points
     global chicken_color
+    pygame.mixer.music.load('data/fon1.mp3')
+
+    end_sound = pygame.mixer.Sound('data/end_sound.wav')
+    fense_sound = pygame.mixer.Sound('data/fense_sound.wav')
+    start_sound = pygame.mixer.Sound('data/start_sound.wav')
+    start_sound.play()
+    count = 0
     screen = pygame.display.set_mode(size)
     running = True
     fences = ["black_fense.png", "green_fense.png", "white_fense.png"]
@@ -207,19 +236,22 @@ def game_screen():
     egg_fox = False
     chicken_color = 0
     screen.blit(load_image("game_fon.png"), (0, 0))
-    chicken = AnimatedSprite(load_image(chickens[chicken_color]), 3, 1, 113, 300)
+    chicken = AnimatedSprite(load_image(chickens[chicken_color]), 3, 1, chicken.rect[0], 300)
     chicken_group = pygame.sprite.Group(chicken)
     USEREVENT = 31
     USEREVENT1 = 23
     pygame.time.set_timer(USEREVENT, 12000)
     pygame.time.set_timer(USEREVENT1, 3000)
     points = 0
-    font = pygame.font.Font(None, 50)
-    text = font.render(str(points), 1, (100, 255, 100))
-    screen.blit(text, (10, 10))
-    pygame.draw.rect(screen, (0, 255, 0), (0, 0, 5, 5))
+    pygame.mixer.music.play()
+    pygame.mixer.music.set_volume(0.5)
     while running:
+        # if points==5:
+        #     count = 1
+        font = pygame.font.Font(None, 50)
+        text = font.render(str(points), 1, (0, 0, 0))
         screen.blit(load_image("game_fon.png"), (0, 0))
+        screen.blit(text, (10, 10))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -245,9 +277,9 @@ def game_screen():
             if event.type == USEREVENT1:
                 egg_fox = True
                 random.shuffle(eggs)
-                random_egg1 = Egg(0, -185, eggs[0])
-                random_egg2 = Egg(100, -185, eggs[1])
-                random_egg3 = Egg(200, -185, eggs[2])
+                random_egg1 = Egg(0, -185, eggs[0],count)
+                random_egg2 = Egg(100, -185, eggs[1],count)
+                random_egg3 = Egg(200, -185, eggs[2],count)
                 egg_group1 = pygame.sprite.Group(random_egg1)
                 egg_group2 = pygame.sprite.Group(random_egg2)
                 egg_group3 = pygame.sprite.Group(random_egg3)
@@ -262,7 +294,14 @@ def game_screen():
                 egg_fox = False
                 pygame.time.set_timer(USEREVENT1, 2600)
             if (random_egg1.update() is False) or (random_egg2.update() is False) or (random_egg3.update() is False):
+                pygame.mixer.music.pause()
+                end_sound.play()
                 lose_screen()
+
+            if (random_egg1.update() is True) or (random_egg2.update() is True) or (random_egg3.update() is True):
+                egg_sound.play()
+                points += 1
+
             random_egg2.update()
             random_egg3.update()
             egg_group1.draw(screen)
@@ -274,8 +313,9 @@ def game_screen():
                 fence_fox = False
                 pygame.time.set_timer(USEREVENT, 12000)
             if random_fence.update() == True:
+                fense_sound.play()
                 chicken_color = fence_color
-                chicken = AnimatedSprite(load_image(chickens[chicken_color]), 3, 1, 113, 300)
+                chicken = AnimatedSprite(load_image(chickens[chicken_color]), 3, 1, chicken.rect[0], 300)
                 chicken_group = pygame.sprite.Group(chicken)
                 chicken_group.update()
             random_fence.update()
